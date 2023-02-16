a065650e3483811776aed0fb8b128c534be1f313
asefaali <asefalearn@gmail.com>
2023-02-16 03:29:25 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/**
+ * main - prints the phrase "Programming is like bulding a multilingual puzzle
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
